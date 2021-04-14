7f0e261cbd515d6195cd517ad6ebef8aa3c6ab6c
BradGillis4 <bradgillis4@gmail.com>
2021-04-14 18:36:18 -0400

    commit tree 97d37337bc39630bcd2ba0503b889cb652568843

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913134502) do
+ActiveRecord::Schema.define(version: 20160108015408) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
+    t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,7 +23,6 @@ ActiveRecord::Schema.define(version: 20180913134502) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "artist_id"
   end
 
 end
